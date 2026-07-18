58a7f71c316b6c4b1f814bc51f63426459f875fc
SHEIKWDS <sheik@whydigit.in>
2026-07-18 16:24:39 +0530

    bug fixing

--- src/menu-items/me.js
+++ src/menu-items/me.js
@@ -110,7 +110,7 @@ const meChildren = [
     id: 'task',
     title: 'Task',
     type: 'item',
-    url: '/team/Task',
+    url: '/me/Task',
     icon: IconFileInvoice
   },
 ].filter(Boolean); // filter out inaccessible screens
